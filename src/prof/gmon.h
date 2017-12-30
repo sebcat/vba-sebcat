254f8bef1b20378a6b5f31632477e60d328988b5
Sebastian Cato <seb.cato@gmail.com>
2017-12-30 13:50:10 +0100

    prof: Do not cast char* to u32

--- src/prof/gmon.h
+++ src/prof/gmon.h
@@ -29,6 +29,8 @@
 #ifndef gmon_h
 #define gmon_h
 
+#include <stdint.h>
+
 /* Size of the 4.4BSD gmon header */
 #define GMON_HDRSIZE_BSD44_32 (4 + 4 + 4 + 4 + 4 + (3 * 4))
 #define GMON_HDRSIZE_BSD44_64 (8 + 8 + 4 + 4 + 4 + (3 * 4))
@@ -119,7 +121,7 @@
 
 struct tostruct
   {
-    char *selfpc;
+    uint32_t selfpc;
     int count;
     unsigned short link;
   };
--- src/prof/prof.cpp
+++ src/prof/prof.cpp
@@ -279,7 +279,7 @@ void profCleanup()
 
 void profCount()
 {
-  register char *selfpc;
+  register u32 selfpc;
   register unsigned short *frompcindex;
   register struct tostruct *top;
   register struct tostruct *prevtop;
@@ -292,7 +292,7 @@ void profCount()
   
   /* selfpc = pc pushed by mcount call.
      This identifies the function that was just entered.  */
-  selfpc = (char *) reg[14].I;
+  selfpc = reg[14].I;
   /* frompcindex = pc in preceding frame.
      This identifies the caller of the function just entered.  */
   frompcindex = (unsigned short *) reg[12].I;
